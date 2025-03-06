9f27e3aee6bd56ef24b4bbe3413c12b056d84db3
Rodrigo Ulir Braz <rulir@hotmail.com>
2025-03-05 21:18:03 -0300

    atualizar agenda

--- src/app/guards/app-routing.module.ts
+++ src/app/guards/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from '../modules/auth/login.component';
+import { DashboardComponent } from '../modules/dashboard/dashboard.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
